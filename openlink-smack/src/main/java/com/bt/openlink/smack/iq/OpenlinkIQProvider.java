54e834236fef1324ce52d94f6a01576fef7b9e1c
Horsley-Rai <marcus.horsley@bt.com>
2018-05-11 11:22:30 +0100

    Adding support for RequestActionResult building & parsing

--- openlink-smack/src/main/java/com/bt/openlink/smack/iq/OpenlinkIQProvider.java
+++ openlink-smack/src/main/java/com/bt/openlink/smack/iq/OpenlinkIQProvider.java
@@ -56,7 +56,8 @@ public class OpenlinkIQProvider extends IQProvider<IQ> {
             new IQMatcher(OpenlinkXmppNamespace.OPENLINK_GET_FEATURES, ATTRIBUTE_STATUS, ACTION_COMPLETED, GetFeaturesResult::from),
             new IQMatcher(OpenlinkXmppNamespace.OPENLINK_MAKE_CALL, ATTRIBUTE_ACTION, ACTION_EXECUTE, MakeCallRequest::from),
             new IQMatcher(OpenlinkXmppNamespace.OPENLINK_MAKE_CALL, ATTRIBUTE_STATUS, ACTION_COMPLETED, MakeCallResult::from),
-            new IQMatcher(OpenlinkXmppNamespace.OPENLINK_REQUEST_ACTION, ATTRIBUTE_ACTION, ACTION_EXECUTE, RequestActionRequest::from)
+            new IQMatcher(OpenlinkXmppNamespace.OPENLINK_REQUEST_ACTION, ATTRIBUTE_ACTION, ACTION_EXECUTE, RequestActionRequest::from),
+            new IQMatcher(OpenlinkXmppNamespace.OPENLINK_REQUEST_ACTION, ATTRIBUTE_STATUS, ACTION_COMPLETED, RequestActionResult::from)
             );
 
     @Override
